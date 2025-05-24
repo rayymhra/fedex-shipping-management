2d77228f0f295252673a611b95abdda72ef36f0e
rayymhra <rayraeilish@gmail.com>
2025-05-24 11:32:02 +0700

    my shipments design page

--- public/my_shipments.php
+++ public/my_shipments.php
@@ -33,14 +33,18 @@ include "../includes/header.php";
 <head>
     <title>My Shipments</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body class="bg-light">
 <div class="container py-5">
-    <h2 class="mb-4">📦 My Assigned Shipments</h2>
-
-    <?php if (mysqli_num_rows($result) > 0): ?>
+    <div class="card">
+        <div class="card-header fedex-purple">
+            My assigned shipments
+        </div>
+        <div class="card-body">
+            <?php if (mysqli_num_rows($result) > 0): ?>
         <table class="table table-bordered table-striped">
-            <thead class="table-dark">
+            <thead class="table-warning">
                 <tr>
                     <th>Tracking Code</th>
                     <th>Customer</th>
@@ -70,7 +74,7 @@ include "../includes/header.php";
                         </td>
                         <td><?= $row['expected_delivery_date'] ?? '-' ?></td>
                         <td>
-                            <a href="update_status.php?id=<?= $row['id'] ?>" class="btn btn-sm btn-outline-primary">
+                            <a href="update_status.php?id=<?= $row['id'] ?>" class="btn btn-sm btn-fedex">
                                 Update Status
                             </a>
                         </td>
@@ -85,6 +89,10 @@ include "../includes/header.php";
     <?php else: ?>
         <div class="alert alert-info">No shipments assigned to you yet.</div>
     <?php endif ?>
+        </div>
+    </div>
+
+    
 </div>
 </body>
 </html>
